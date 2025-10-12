8d346c3295c90faf7213e16b67bcee171e0f4905
Seth Millán <millanseth@gmail.com>
2025-10-11 22:09:50 -0600

    ajustes en ventas y detalle ventas para deploy

--- src/detalle-ventas/entities/detalle-venta.entity.ts
+++ src/detalle-ventas/entities/detalle-venta.entity.ts
@@ -1,10 +1,10 @@
 import { Prisma } from '@prisma/client';
 
-export class DetalleVenta implements Prisma.detalleventasGetPayload<{}> {
+export class DetalleVenta {
   id: number;
   venta_id: number;
   inventario_id: number;
   cantidad: number;
-  total: Prisma.Decimal;
+  total: number;
   created_at: Date;
 }
--- src/ventas/entities/venta.entity.ts
+++ src/ventas/entities/venta.entity.ts
@@ -1,15 +1,15 @@
 import { Prisma, tipo_pago_enum, tipo_venta_enum } from '@prisma/client';
 
-export class Venta implements Prisma.ventasGetPayload<{}> {
+export class Venta {
   id: number;
   fecha: Date;
   tipo_venta: tipo_venta_enum;   
   tipo_pago: tipo_pago_enum;     
   cliente_id: number | null;
   empleado_id: number;
-  subtotal: Prisma.Decimal;
-  descuento: Prisma.Decimal;
-  total: Prisma.Decimal;
+  subtotal: number;
+  descuento: number;
+  total: number;
   created_at: Date;
   updated_at: Date;
 }
