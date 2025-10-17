cc9d2aa891f329d0e46742ff92dcda5fe56f7736
AdrianMO <adrian.martinez.260821@gmail.com>
2025-10-16 22:04:01 -0600

    docs: :art: Documentacion con scalar

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +16,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('BackTennis')
     .build();
-const documentFactory = () => SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('docs', app, documentFactory());
+const document = SwaggerModule.createDocument(app, config);
+app.use('/docs', apiReference({content:document}))
 
   await app.listen(process.env.PORT ?? 3000);
 }
