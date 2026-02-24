8dcf157a7384532a26d66c32379cf78b2dab806b
Seth Millán <millanseth@gmail.com>
2026-02-23 21:36:09 -0600

    fix: :sparkles: Response entidad domicilio
    parece que todo bien browsky

--- src/domicilios/domicilios.controller.ts
+++ src/domicilios/domicilios.controller.ts
@@ -1,34 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  UseGuards,
+} from '@nestjs/common';
 import { DomiciliosService } from './domicilios.service';
 import { CreateDomicilioDto } from './dto/create-domicilio.dto';
 import { UpdateDomicilioDto } from './dto/update-domicilio.dto';
+import { AuthGuard } from 'src/Auth/guard/auth.guard';
+import { plainToInstance } from 'class-transformer';
+import { DomicilioResponseDto } from './dto/response-domicilio.dto';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('domicilios')
 export class DomiciliosController {
   constructor(private readonly domiciliosService: DomiciliosService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   async create(@Body() createDomicilioDto: CreateDomicilioDto) {
-    return await this.domiciliosService.create(createDomicilioDto);
+    const domicilioEntity= await this.domiciliosService.create(createDomicilioDto);
+    return plainToInstance(DomicilioResponseDto, domicilioEntity, {
+      excludeExtraneousValues: true, 
+    });
   }
 
   @Get()
   async findAll() {
-    return await this.domiciliosService.findAll();
+    const domicilioEntity =  await this.domiciliosService.findAll();
+    return plainToInstance(DomicilioResponseDto, domicilioEntity, {
+      excludeExtraneousValues: true, 
+    });
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return await this.domiciliosService.findOne(+id);
+    const domicilioEntity = await this.domiciliosService.findOne(+id);
+    return plainToInstance(DomicilioResponseDto, domicilioEntity, {
+      excludeExtraneousValues: true, 
+    });
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateDomicilioDto: UpdateDomicilioDto) {
-    return await this.domiciliosService.update(+id, updateDomicilioDto);
+  @UseGuards(AuthGuard)
+  async update(
+    @Param('id') id: string,
+    @Body() updateDomicilioDto: UpdateDomicilioDto,
+  ) {
+    const domicilioEntity = await this.domiciliosService.update(+id, updateDomicilioDto);
+    return plainToInstance(DomicilioResponseDto, domicilioEntity, {
+      excludeExtraneousValues: true, 
+    });
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   async remove(@Param('id') id: string) {
-    return await this.domiciliosService.remove(+id);
+    const domicilioEntity = await this.domiciliosService.remove(+id);
+    return plainToInstance(DomicilioResponseDto, domicilioEntity, {
+      excludeExtraneousValues: true, 
+    });
   }
 }
